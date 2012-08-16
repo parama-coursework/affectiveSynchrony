2d6c6d30359a4cea8b87d693213cef941204db7f
Parama Pal <paramap@gmail.com>
2012-08-16 04:33:51 -0400

    added functional plot

---
+++ vis/parseData.js
@@ -0,0 +1,26 @@
+//Data Parsing Functions
+
+//take in data array and output in the right x y format for rickshaw
+//this method applies the time constant for the given data. 
+
+var timeConstant = 1/120;
+
+function convertArrayCoordsWithTime(a){
+	
+	var dataArray = [];
+	var newX=0;
+	var newY=0;
+	var newPoint=0;
+	var nume = 0;
+	var denom = 2; //current time constant - half a second
+	for(i=0;i<a.length;i++)
+	{
+		nume = i;
+		newX= nume/denom;
+		
+		newPoint = { x:newX, y:a[i] };
+		dataArray.push(newPoint);
+	}
+	
+	return dataArray;
+}
